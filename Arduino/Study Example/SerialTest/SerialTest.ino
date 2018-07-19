511d5fdfacaafc44ed73cee47e20c2b350341f79
jinzzam <firstmind_@naver.com>
2018-07-20 02:47:12 +0900

    delete serial code

--- Arduino/Study Example/SerialTest/SerialTest.ino
+++ Arduino/Study Example/SerialTest/SerialTest.ino
@@ -1,13 +1,10 @@
-int val = 1;
 const int LED = 7;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  Serial.println(val++);
   digitalWrite(LED, HIGH);
   delay(800);
   digitalWrite(LED, LOW);
